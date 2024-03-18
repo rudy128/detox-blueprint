38240b14a1a2fb2191a44e2fbdf25bd8974d0421
Rudy <prathamkamthan1306@gmail.com>
2024-03-18 16:21:43 +0530

    Just a bit of styling

--- app/components/P1.tsx
+++ app/components/P1.tsx
@@ -2,15 +2,14 @@ import React from 'react'
 
 const P1 = () => {
   return (
-    <div className='w-5/6 my-20 space-y-8 flex flex-col items-center'>
-        <h1 className='text-8xl'>Forget about all the treatments and medicine. This is the only short-cut to health</h1>
+    <div className='w-5/6 my-20 space-y-8 flex flex-col items-center bg-[#131b23] p-10 rounded-md'>
+        <h1 className='text-6xl'>Forget about all the treatments and medicine. This is the only short-cut to health</h1>
         <h3 className='text-3xl'>If I'd tell you that the modern health care system isn't out there to fix your health Problems but only suppress it? Would you believe me?</h3>
         <div className='flex justify-center space-x-16'>
-            <div className='h-[300px] w-[500px] flex justify-center items-center text-9xl cursor-pointer'>Video</div>
-            <div className='flex flex-col items-center justify-between'><h3>If you're somebody who cares for their well-being and understands it's importance then pay attention, this is very important. Chance has it that you struggle mental disorders or problems with health. In short, your body is screaming at you "Can I please Have a break?!". Your body doesn't want drugs, medicine or shots. It wants to be clean from that or in other words Cleansed. While I still have your attention let me introduce myself with the video ------ </h3><button>Pay wall</button></div>
+            <div className='h-[300px] w-[500px] flex justify-center items-center text-4xl cursor-pointer'>Video</div>
+            <div className='w-[400px] flex flex-col items-center justify-between'><h3>If you're somebody who cares for their well-being and understands it's importance then pay attention, this is very important. Chance has it that you struggle mental disorders or problems with health. In short, your body is screaming at you "Can I please Have a break?!". Your body doesn't want drugs, medicine or shots. It wants to be clean from that or in other words Cleansed. While I still have your attention let me introduce myself with the video ------ </h3><button>Pay wall</button></div>
         </div>
     </div>
   )
 }
-Penis
 export default P1
